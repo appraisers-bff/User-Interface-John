2fbc02c76c77637df10ab0a13b12caf057e50920
jvgiv <jvgiv12@gmail.com>
2019-03-11 22:48:18 -0400

    some animations added

---
+++ js/java.js
@@ -0,0 +1,5 @@
+TweenMax.from('.header__text', 1.5, {x:-300, opacity:0, scale:0.5, /*onComplete: tweener*/});
+
+// function tweener() {
+    TweenMax.from('.header__button', 1.5, {x:300, opacity:0, scale:0.5});
+// }
